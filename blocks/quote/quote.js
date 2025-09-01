2d2f9805825afe71cfbfa724bbf16045e8ef57ec
rahulrawatgspann <rahul.rawat@gspann.com>
2025-09-01 13:29:31 +0530

    tailwind fix4

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -8,7 +8,4 @@ export default function decorate(block) {
   blockquote.classList.add('bg-blue-500', 'text-white', 'px-4', 'py-2');
 
   quoteWrapper.replaceChildren(blockquote);
-
-  // Test with inline styles to confirm Tailwind utilities exist
-  block.style.cssText = 'background-color: rgb(254 249 195) !important; border-left: 4px solid rgb(234 179 8) !important; padding: 1rem !important;';
 }

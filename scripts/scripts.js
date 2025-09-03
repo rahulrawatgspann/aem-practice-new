f40fb5ddce1d3f7c6afdd67e36fec80099660289
rahulrawatgspann <rahul.rawat@gspann.com>
2025-09-03 12:10:22 +0530

    header fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -149,7 +149,7 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header-test'));
+  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
